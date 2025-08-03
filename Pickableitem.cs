f27758b8ba02032a9e8b06d06a3fa0fafd5ba4cc
DAF <91028158+MistiikDev@users.noreply.github.com>
2025-08-03 21:33:32 +0200

    Further Improved Player Inventory System. Fixed Bullet Rendering Issues

--- Pickableitem.cs
+++ Pickableitem.cs
@@ -2,21 +2,29 @@ using UnityEngine;
 
 public abstract class Item : MonoBehaviour
 {
+    public bool b_equipOnPick = false;
+
     public string Name;
     public uint ID;
     private bool Equipped;
-
-    private GameObject gameObject;
+    private bool isActive;
+    
+    private GameObject itemGameObject;
     private Rigidbody rigidBody;
 
     public bool b_isEquipped()
     {
         return Equipped;
     }
+
+    public bool b_isActive()
+    {
+        return isActive;
+    }
     
     public GameObject GetGameObject()
     {
-        return this.gameObject;
+        return this.itemGameObject;
     }
 
     public Rigidbody GetBody()
@@ -26,26 +34,44 @@ public abstract class Item : MonoBehaviour
 
     public void SetGameObject(GameObject gameObject)
     {
-        this.gameObject = gameObject;
+        this.itemGameObject = gameObject;
     }
     
-    public abstract void OnPick(Player player);
-    public abstract void OnDrop();
-    public abstract void Use();
-    
-    public void Equip()
+    public abstract void Picked(Player player);
+    public abstract void Dropped();
+
+    public void OnPick(Player player)
     {
         this.Equipped = true;
+        
         if (rigidBody)
         {
             Destroy(rigidBody);
         }
-    }
 
-    public void Unequip()
+        Picked(player);
+    }
+    
+    public void OnDrop()
     {
         this.Equipped = false;
         
         rigidBody = this.gameObject.AddComponent<Rigidbody>();
+        rigidBody.interpolation = RigidbodyInterpolation.Interpolate;
+        
+        Dropped();
+    }
+    public abstract void Use();
+    
+    public void Equip()
+    {
+        this.isActive = true;
+        this.gameObject.SetActive(true);
+    }
+
+    public void Unequip()
+    {
+        this.isActive = false;
+        this.gameObject.SetActive(false);
     }
 }
